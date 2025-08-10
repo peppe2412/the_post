222a51a3dc14fbbbd9ffd2010370704af950a879
peppe2412 <peppeterrazzano79@gmail.com>
2025-08-10 19:28:50 +0200

    scaffolding fatto

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(PublicController::class)->group(function (){
+    Route::get('/', 'home')->name('home');
 });
